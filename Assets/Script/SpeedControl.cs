f3488ce619a12b76fb80c5a9436a33e9ba7a0237
Cem Demirtas <97852548+cemdemirtas@users.noreply.github.com>
2022-06-01 23:14:28 +0300

    Speed ProgressBar has fixed

--- Assets/Script/SpeedControl.cs
+++ Assets/Script/SpeedControl.cs
@@ -6,20 +6,22 @@ using TMPro;
 public class SpeedControl : MonoBehaviour
 {
     public Slider speedSlider;
-    [SerializeField] float speed = 0;
+    [SerializeField] float speed;
     [SerializeField] TextMeshProUGUI Speedtxt;
     [SerializeField] Image SpeedProgress ;
     private void Start()
     {
         speedSlider.value = 0;
+        speed = 0;
     }
 
     private void Update()
     {
         //If fit for you, i would like using slider instead of gas or break pedal.
 
-        speed = speedSlider.value;
         AircraftMovement.instance.forwardSpeed = speed;
+        speed = speedSlider.value;
+
         if (speedSlider.value>1)
         {
             transform.GetComponent<Rigidbody>().isKinematic = false;
@@ -28,7 +30,7 @@ public class SpeedControl : MonoBehaviour
 
         }
         Speedtxt.text = "Speed: "+ Mathf.Round(AircraftMovement.instance.forwardSpeed*40).ToString(); // reach speed value using by singleton
-        SpeedProgress.fillAmount = (AircraftMovement.instance.forwardSpeed * 40) / 800;
+        SpeedProgress.fillAmount = (AircraftMovement.instance.forwardSpeed *40) / 800;
     }
 
 
